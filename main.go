f2995be1d65e3a7a5a95fa276a0e0d9d00d12249
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-04-21 22:46:28 -0700

    drawing a rectangle

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 type Game struct{}
+
+var white *ebiten.Image
+
+func init() {
+  white, _ = ebiten.NewImage(1, 1, ebiten.FilterDefault);
+	white.Fill(color.RGBA{0xff, 0xff, 0xff, 0xff})
+}
 
 func (g *Game) Update(screen *ebiten.Image) error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+  op := &ebiten.DrawImageOptions{}
+  op.GeoM.Scale(20, 20);
+  op.GeoM.Translate(100, 100);
+  screen.DrawImage(white, op);
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
