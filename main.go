262ac81a0a04edd78eb8609e8ef5d738d094246d
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-04-21 23:26:46 -0700

    getting ready for bench!

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 type Game struct{}
@@ -13,7 +15,7 @@ var white *ebiten.Image
 
 func init() {
 	white, _ = ebiten.NewImage(1, 1, ebiten.FilterDefault)
-	white.Fill(color.RGBA{0xff, 0xff, 0xff, 0xff})
+	white.Fill(color.White)
 }
 
 func (g *Game) Update(screen *ebiten.Image) error {
@@ -24,7 +26,10 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Scale(20, 20)
 	op.GeoM.Translate(100, 100)
+	op.ColorM.Scale(1, 0, 0, 1)
 	screen.DrawImage(white, op)
+
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("fps: %0.2f", ebiten.CurrentFPS()))
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
