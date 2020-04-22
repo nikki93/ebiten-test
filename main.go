71057d1ea1b137027b973066852a85678205be96
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-04-22 00:10:07 -0700

    game struct

--- main.go
+++ main.go
@@ -15,9 +15,7 @@ import (
 const W = 947
 const H = 781
 
-var white *ebiten.Image
-
-const N = 30000
+const N = 1000
 
 type rect struct {
 	x, y    float64
@@ -27,9 +25,13 @@ type rect struct {
 	phase   float64
 }
 
-var rects = [N]*rect{}
+type game struct {
+	loaded bool
 
-type game struct{}
+	white *ebiten.Image
+
+	rects [N]*rect
+}
 
 var startTime time.Time
 var lastFrameTime time.Time
@@ -38,11 +40,11 @@ func (g *game) Load() {
 	startTime = time.Now()
 	lastFrameTime = startTime
 
-	white, _ = ebiten.NewImage(1, 1, ebiten.FilterDefault)
-	white.Fill(color.White)
+	g.white, _ = ebiten.NewImage(1, 1, ebiten.FilterDefault)
+	g.white.Fill(color.White)
 
 	for i := 0; i < N; i++ {
-		rects[i] = &rect{
+		g.rects[i] = &rect{
 			x:     W * rand.Float64(),
 			y:     H * rand.Float64(),
 			r:     rand.Float64(),
@@ -54,15 +56,21 @@ func (g *game) Load() {
 			phase: 2 * math.Pi * rand.Float64(),
 		}
 	}
+
+	g.loaded = true
 }
 
 func (g *game) Update(screen *ebiten.Image) error {
+	if !g.loaded {
+		g.Load()
+	}
+
 	now := time.Now()
 	t := now.Sub(startTime).Seconds()
 	dt := now.Sub(lastFrameTime).Seconds()
 
 	for i := 0; i < N; i++ {
-		rect := rects[i]
+		rect := g.rects[i]
 		rect.x += rect.speed * math.Sin(t+rect.phase) * dt
 	}
 
@@ -73,13 +81,13 @@ func (g *game) Update(screen *ebiten.Image) error {
 func (g *game) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 	for i := 0; i < N; i++ {
-		rect := rects[i]
+		rect := g.rects[i]
 		op.GeoM.Reset()
 		op.ColorM.Reset()
 		op.GeoM.Scale(rect.w, rect.h)
 		op.GeoM.Translate(rect.x, rect.y)
 		op.ColorM.Scale(rect.r, rect.g, rect.b, 1)
-		screen.DrawImage(white, op)
+		screen.DrawImage(g.white, op)
 	}
 
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("fps: %0.2f", ebiten.CurrentFPS()))
@@ -92,9 +100,7 @@ func (g *game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(W, H)
 	ebiten.SetWindowTitle("Hello, World!")
-	g := &game{}
-	g.Load()
-	if err := ebiten.RunGame(g); err != nil {
+	if err := ebiten.RunGame(&game{}); err != nil {
 		log.Fatal(err)
 	}
 }
