ed6a374c7c45a940d2a0389cc5cc342d296b1798
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-04-21 23:01:57 -0700

    formatting

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ type Game struct{}
 var white *ebiten.Image
 
 func init() {
-  white, _ = ebiten.NewImage(1, 1, ebiten.FilterDefault);
-	white.Fill(color.RGBA{0xff, 0xff, 0xff, 0xff})
+	white, _ = ebiten.NewImage(1, 1, ebiten.FilterDefault)
+	white.Fill(color.RGBA{0xff, 0, 0xff, 0xff})
 }
 
 func (g *Game) Update(screen *ebiten.Image) error {
@@ -21,10 +21,10 @@ func (g *Game) Update(screen *ebiten.Image) error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-  op := &ebiten.DrawImageOptions{}
-  op.GeoM.Scale(20, 20);
-  op.GeoM.Translate(100, 100);
-  screen.DrawImage(white, op);
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(20, 20)
+	op.GeoM.Translate(100, 100)
+	screen.DrawImage(white, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
