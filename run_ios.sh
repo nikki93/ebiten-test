4a77e204b9cdbec0d370e6483b9524bff549a33c
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2020-04-26 23:23:53 -0700

    update ios run script

--- run_ios.sh
+++ run_ios.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+set -e
+
 pushd mobile
-time go run github.com/nikki93/gomob -x -target ios -iosheader ios.h -o ../ios/Mobile.framework .
+go run github.com/nikki93/gomob -target ios -iosheader ios.h -o ../ios/Mobile.framework .
 popd
 
 pushd ios
-xcodebuild
+xcodebuild | xcpretty
 popd
 
 ios-deploy -L -b ios/build/Release-iphoneos/*.app
